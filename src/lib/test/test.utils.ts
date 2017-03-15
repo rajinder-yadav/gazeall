b08da1b8c7acca4f7849f9ea05063f886f540db5
Rajinder Yadav <rajinder.yadav@hotmail.com>
2017-03-14 21:56:56 -0400

    Updated tslint rules.

--- src/lib/test/test.utils.ts
+++ src/lib/test/test.utils.ts
@@ -1,5 +1,5 @@
 import * as test from "tape";
-import * as utils from "../utils";
+// import * as utils from "../utils";
 
 test( "Gazeall", t => {
   t.equal( "Watching gazeall run!", "Watching gazeall run!" );
