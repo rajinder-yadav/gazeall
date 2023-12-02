f2c7b98010d800e2f06b685f63f43504376fd550
Dev Guy <devguy.ca@gmail.com>
2023-12-02 14:36:58 -0500

    fix: reading package.json file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@
 import process from 'node:process';
 
 import {Command} from 'commander';
-import {watchAndRun, StopLaunchedProcesses} from './lib/gazeall';
+import {
+  watchAndRun,
+  StopLaunchedProcesses,
+  readPackageJSONProperties,
+} from './lib/gazeall';
 
 /** Handle signal when User pressed CTRL+C to stop gazeall.*/
 process.on('SIGINT', function (code) {
@@ -16,24 +20,27 @@ const cmd = new Command();
 
 /** Command options. */
 cmd
-  .version('gazeall version: 0.11.0', '-v, --version')
+  .version('gazeall version: 0.11.1', '-v, --version')
   .usage('[options] [files...]')
   .option(
     '-r, --run <command...>',
-    'run commands then wait for changes to re-run.',
+    'run commands then wait for changes to re-run',
   )
-  .option('-w, --watch <files...>', 'files to watch for change.')
-  .option('-W, --wait', 'enter wait, commands will run on changes.')
-  .option('-p, --npmp <scripts>', 'NPM scripts to run parallel.')
-  .option('-s, --npms <scripts>', 'NPM scripts to run synchronous.')
-  .option('-d, --delay <ms>', 'start delay value in milliseconds.')
-  .option('-H, --halt', 'halt on error.')
+  .option('-w, --watch <files...>', 'files to watch for change')
+  .option('-W, --wait', 'enter wait, commands will run on changes')
+  .option('-p, --npmp <scripts>', 'NPM scripts to run parallel')
+  .option('-s, --npms <scripts>', 'NPM scripts to run synchronous')
+  .option('-d, --delay <ms>', 'start delay value in milliseconds')
+  .option('-H, --halt', 'halt on error')
+  .option('-V, --verbose', 'verbose logging')
   .parse(process.argv);
 
 const options = cmd.opts();
 options.args = cmd.args;
 // console.log('debug [options]> ', options); // !debug
 // console.log('debug [cmd.args]> ', cmd.args); // !debug
+
+options.PACKAGE_JSON = readPackageJSONProperties();
 
 /** Start watching after delay interval or next event loop if value not provided. */
 setTimeout(
