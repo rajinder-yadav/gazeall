7a8028b0ce931f9fdbb81f2ba433d44537219add
Dev Guy <devguy.ca@gmail.com>
2023-12-06 01:03:11 -0500

    feat: v0.13.0

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ const cmd = new Command();
 
 /** Command options. */
 cmd
-  .version('gazeall version: 0.12.2', '-v, --version')
+  .version('gazeall version: 0.13.0', '-v, --version')
   .usage('[options] [files...]')
   .option(
     '-r, --run <command...>',
