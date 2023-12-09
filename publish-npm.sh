050b0700d7ba40cd438e0fb5aba9abb52b65d28a
Dev Guy <devguy.ca@gmail.com>
2023-12-09 04:04:06 -0500

    edit: updated

--- publish-npm.sh
+++ publish-npm.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 npm login --registry https://registry.npmjs.org
 npm publish
+
+if [ $? -eq 0 ]; then
+  echo "Gazeall has been published to NPM successfully"
+  echo "https://www.npmjs.com/package/gazeall"
+else
+  echo "!!! ERROR: Something went wrong"
+fi
