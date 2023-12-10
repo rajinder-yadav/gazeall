bb87d15d2a69fa4cef4e4465c3e1caeaba8a9e56
Dev Guy <devguy.ca@gmail.com>
2023-12-09 21:54:08 -0500

    edit: error message

--- publish-npm.sh
+++ publish-npm.sh
@@ -6,5 +6,5 @@ if [ $? -eq 0 ]; then
   echo "Gazeall has been published to NPM successfully"
   echo "https://www.npmjs.com/package/gazeall"
 else
-  echo "!!! ERROR: Something went wrong"
+  echo "ERROR: Something went wrong"
 fi
